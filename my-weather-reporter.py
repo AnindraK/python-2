01c572f3fd91c4725c4b6cbabb89d1ccce607278
AnindraK <karanindra304@gmail.com>
2026-05-31 15:32:37 +0530

    Add files via upload

---
+++ my-weather-reporter.py
@@ -0,0 +1,19 @@
+city = input("Write you city name:")
+temp = float(input("Write your temp here in C:"))
+if temp > 35:
+    print("Warning: It is very hot today!")
+
+if temp > 25:
+        print("It is a great day to go outside!")
+else:
+        print("Grab a jacket before u go out!")
+
+if temp > 35:
+       print("Warning: it is schorching hot!")
+if temp >25:
+       print("Weather: warm and sunny!")
+if temp > 15:
+       print("weather: cool and brezzy!")
+else:
+       print("Weather: Cold- STAY WARM!")
+    
